01dfe8ccea7319323670360fefc1d7c26419270d
andreyk1n <betas23wr@gmail.com>
2026-05-14 13:53:05 +0300

    upd index.php

--- index.php
+++ index.php
@@ -6,7 +6,17 @@ Template Name: Front Page
 <?php get_header(); ?>
 
 <main>
-    site
+    <?php
+    if (have_rows('flexible_content')):
+        while (have_rows('flexible_content')):
+            the_row();
+            get_template_part(
+                'template-parts/components/' . get_row_layout()
+            );
+
+        endwhile;
+    endif;
+    ?>
 </main>
 
 <?php get_footer(); ?>
